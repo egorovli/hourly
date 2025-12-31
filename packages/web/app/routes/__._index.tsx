dd18ac3aa0bc5224ac97df81edf4370ab6d5cf3d
Anton Egorov <anton@egorov.io>
2025-12-31 11:43:18 +0100

    feat: add external drag-and-drop for issues onto calendar
    Because apparently we needed another way to drag things around. Adds FullCalendar interaction plugin and implements draggable issue items in a virtualized sidebar that can be dropped onto the calendar. Because clicking is too mainstream.
    
    References #59

--- packages/web/app/routes/__._index.tsx
+++ packages/web/app/routes/__._index.tsx
@@ -1,42 +1,135 @@
+import type { PluginDef } from '@fullcalendar/core'
 import type { MetaDescriptor } from 'react-router'
 import type { Route } from './+types/__._index.ts'
-import type { DatesSetArg, EventInput } from '@fullcalendar/core'
-//
-import FullCalendar from '@fullcalendar/react'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import { Suspense, useEffect, useState, useMemo } from 'react'
+import type { Draggable } from '@fullcalendar/interaction'
 
-import { lazy } from 'react'
+import { Suspense, useEffect, useRef, useState, lazy } from 'react'
+import { Virtuoso } from 'react-virtuoso'
 
-// const FullCalendar = lazy(() => import('@fullcalendar/react'))
+import { invariant } from '~/lib/util/index.ts'
+
+const FullCalendar = lazy(() => import('@fullcalendar/react'))
 
 export default function IndexPage(): React.ReactNode {
 	const [displayCalendar, setDisplayCalendar] = useState(false)
+	const pluginsRef = useRef<PluginDef[]>([])
+	const draggableConstructorRef = useRef<typeof Draggable>(null)
 
 	useEffect(() => {
-		setTimeout(() => {
-			setDisplayCalendar(true)
-		}, 1000)
+		Promise.all([
+			import('@fullcalendar/timegrid')
+				.then(module => module.default)
+				.then(module => {
+					pluginsRef.current.push(module)
+				}),
+
+			import('@fullcalendar/interaction')
+				.then(module => module.default)
+				.then(module => {
+					pluginsRef.current.push(module)
+				}),
+
+			import('@fullcalendar/interaction')
+				.then(module => module.Draggable)
+				.then(module => {
+					draggableConstructorRef.current = module
+				})
+		])
+			.then(() => {
+				setDisplayCalendar(true)
+				console.log(pluginsRef.current)
+				console.log(draggableConstructorRef.current)
+			})
+			.catch(err => {
+				console.error(err)
+			})
 	}, [])
 
 	if (!displayCalendar) {
 		return null
 	}
 
+	// invariant(draggableConstructorRef.current, 'Draggable constructor not found')
+
 	return (
-		<div className='w-full h-full'>
-			<Suspense fallback={<div>Loading...</div>}>
-				<FullCalendar
-					plugins={[timeGridPlugin]}
-					initialView='timeGridWeek'
-					height='100%'
-					events={[]}
-					datesSet={() => {}}
-					firstDay={1}
+		<div className='h-full flex flex-row gap-4 p-4'>
+			<div className='flex flex-col gap-4 border'>
+				<h2 className='text-2xl font-bold'>Issues</h2>
+				<Virtuoso
+					// style={{ height: '100%' }}
+
+					totalCount={200}
+					itemContent={index => {
+						invariant(draggableConstructorRef.current, 'Draggable constructor not found')
+
+						return (
+							<IssueItem
+								id={index.toString()}
+								name={`Issue ${index}`}
+								components={{
+									Draggable: draggableConstructorRef.current
+								}}
+							/>
+						)
+					}}
 				/>
-			</Suspense>
+			</div>
+			<div className='flex flex-col grow gap-4'>
+				<h2 className='text-2xl font-bold'>Calendar</h2>
+				<Suspense fallback={<div>Loading...</div>}>
+					<FullCalendar
+						plugins={pluginsRef.current}
+						initialView='timeGridWeek'
+						height='100%'
+						events={[]}
+						datesSet={() => {}}
+						firstDay={1}
+						editable
+						droppable
+					/>
+				</Suspense>
+			</div>
 		</div>
 	)
+}
+
+interface IssueItemProps {
+	id: string
+	name: string
+	components: IssueItemComponents
+}
+
+interface IssueItemComponents {
+	Draggable: typeof Draggable
+}
+
+function IssueItem(props: IssueItemProps): React.ReactNode {
+	const ref = useRef<HTMLDivElement>(null)
+	const Draggable = props.components.Draggable
+	const draggableRef = useRef<Draggable>(null)
+
+	useEffect(() => {
+		if (!ref.current) {
+			return
+		}
+
+		const draggable = new Draggable(ref.current, {
+			eventData(el) {
+				return {
+					id: props.id,
+					duration: '01:30'
+				}
+			}
+		})
+
+		draggableRef.current = draggable
+
+		return () => {
+			draggable.destroy()
+		}
+	}, [Draggable, props.id])
+
+	return <div ref={ref}>Item {props.name}</div>
 }
 
 export async function loader({ request }: Route.LoaderArgs) {
